54fdb7fb1a28337d9097e762e2118e8eaecc934f
Moritz Molch <moritz.molch@cern.ch>
2025-08-15 15:25:12 +0200

    Add test script for compiling the analysis in 2022preEE

---
+++ build_scripts/test_build_2022preEE.sh
@@ -0,0 +1,68 @@
+#!/usr/bin/env bash
+
+
+
+main () {
+    # get paths of the script and its directory
+    local this_file="$( echo "${BASH_SOURCE[0]}" )"
+    local this_dir="$( cd "$( dirname "${this_file}" )" && pwd )"
+
+    # get the CROWN directories
+    local crown_dir="$( cd "${this_dir}/../.." && pwd )"
+    local crown_build_dir="${crown_dir}/build"
+    local crown_bin_dir="${crown_build_dir}/bin"
+
+    # create the build directory if it does not exist
+    if [[ ! -d "${crown_build_dir}" ]]; then
+        mkdir -p "${crown_build_dir}"
+    fi
+
+    # define fixed parameters of the compilation process
+    local analysis="xyh_bbtautau"
+    local config="nmssm_config"
+    local era="2022preEE"
+    local shifts="none"
+    local threads="$( [[ "${debug}" == true ]] && echo "1" || echo "4" )"
+    local cores="8"
+
+    # split the samples string into an array
+    if [[ "${samples}" == "all" ]]; then
+        samples="ttbar,data,dyjets_amcatnlo"
+    fi
+    declare -a samples_list
+    IFS="," read -ra samples_list <<< "${samples}"
+
+    # build associative array of test files for different sample types
+    declare -A test_files_list
+    test_files_list[ttbar]="root://xrootd-cms.infn.it///store/mc/Run3Summer22NanoAODv12/TTto2L2Nu_TuneCP5_13p6TeV_powheg-pythia8/NANOAODSIM/130X_mcRun3_2022_realistic_v5-v2/2520000/66b834d6-61f7-4109-b5ae-54a150d4814b.root"
+    test_files_list[data]="root://xrootd-cms.infn.it///store/data/Run2022C/EGamma/NANOAOD/22Sep2023-v1/2530000/009621ee-c6b2-4043-99c3-7cb4ab2c7264.root"
+    test_files_list[dyjets_amcatnlo]="root://xrootd-cms.infn.it///store/mc/Run3Summer22NanoAODv12/DYto2Tau-2Jets_MLL-50_0J_TuneCP5_13p6TeV_amcatnloFXFX-pythia8/NANOAODSIM/130X_mcRun3_2022_realistic_v5-v1/120000/fc3c9072-ac0d-4097-9e15-f1a537efee41.root"
+
+    # configure and compile the project
+    (
+        cd "${crown_build_dir}" \
+        && cmake .. -DANALYSIS="${analysis}" -DCONFIG="${config}" -DERAS="${era}" -DSAMPLES="${samples}" -DSCOPES="${channels}" -DSHIFTS="${shifts}" -DTHREADS="${threads}" -DDEBUG="${debug}" \
+        && make -j "${cores}" \
+        && make install
+    ) || return "${?}"
+
+    # test the compiled binary with a test file for each declared sample
+    for sample in "${samples_list[@]}"; do
+        if [[ -n "${test_files_list[${sample}]}" ]]; then
+            # specify paths of the input and the output files
+            test_file="${test_files_list[${sample}]}"
+            output_file="output_${sample}.root"
+
+            # test the binary
+            (
+                cd "${crown_bin_dir}" \
+                && "./${config}_${sample}_${era}" "${output_file}" "${test_file}"
+            ) || return "${?}"
+
+        fi
+    done
+
+}
+
+
+main "${@}"
