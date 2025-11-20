472f9d17840c06d018272bbfc8bd27df5bec331b
Moritz Molch <moritz.molch@cern.ch>
2025-11-20 13:02:21 +0100

    Add C++ producers for jet pt regression with ParticleNet

--- cpp_addons/src/jets.cxx
+++ cpp_addons/src/jets.cxx
@@ -113,7 +113,78 @@ ROOT::RDF::RNode CorrectJetIDRun3NanoV12(
     );
 }
 
+
+ROOT::RDF::RNode JetPtPNetRegressionWithNeutrino(
+    ROOT::RDF::RNode df,
+    const std::string &outputname,
+    const std::string &jet_pt_uncorrected,
+    const std::string &jet_raw_factor,
+    const std::string &jet_pnet_reg_pt_factor,
+    const std::string &jet_pnet_reg_pt_neutrino_factor
+) {
+    auto correction = [] (
+        const ROOT::RVec<float> &jet_pt_uncorrected,
+        const ROOT::RVec<float> &jet_raw_factor,
+        const ROOT::RVec<float> &jet_pnet_reg_pt_factor,
+        const ROOT::RVec<float> &jet_pnet_reg_pt_neutrino_factor
+    ) {
+        // Jet_rawFactor is 1 - (raw pt)/(corrected pt) (from NANOAOD documentation)
+        // Calculate raw pt before JEC
+        auto jet_pt_raw = jet_pt_uncorrected * (1 - jet_raw_factor);
+        auto jet_pt_pnet_neutrino = jet_pt_raw * jet_pnet_reg_pt_factor * jet_pnet_reg_pt_neutrino_factor;
+
+        return jet_pt_pnet_neutrino;
+    };
+
+    return df.Define(
+        outputname,
+        correction,
+        {
+            jet_pt_uncorrected,
+            jet_raw_factor,
+            jet_pnet_reg_pt_factor,
+            jet_pnet_reg_pt_neutrino_factor
+        },
+    );
+};
+
+
+ROOT::RDF::RNode JetPtPNetRegressionResolution(
+    ROOT::RDF::RNode df,
+    const std::string &outputname,
+    const std::string &jet_pt_uncorrected,
+    const std::string &jet_raw_factor,
+    const std::string &jet_pnet_reg_pt_resolution_factor
+) {
+
+    auto resolution = [] (
+        const ROOT::RVec<float> &jet_pt_uncorrected,
+        const ROOT::RVec<float> &jet_raw_factor,
+        const ROOT::RVec<float> &jet_pnet_reg_pt_resolution_factor
+    ) {
+        // Jet_rawFactor is 1 - (raw pt)/(corrected pt) (from NANOAOD documentation)
+        // Calculate raw pt before JEC
+        auto jet_pt_raw = jet_pt_uncorrected * (1 - jet_raw_factor);
+        auto jet_pt_pnet_res = jet_pt_raw * jet_pnet_reg_pt_resolution_factor;
+
+        return jet_pt_pnet_res;
+    };
+
+    // redefine the data type of the Jet ID mask
+    return df.Define(
+        outputname,
+        resolution,
+        {
+            jet_pt_uncorrected,
+            jet_raw_factor,
+            jet_pnet_reg_pt_resolution_factor
+        }
+    );
+};
+
+
 } // end quantities
+
 
 } // end jet
 
