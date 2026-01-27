0d4e2cb44c60ab8ec19f1442b878b0673918c357
moritzmolch <moritz.molch@kit.edu>
2026-01-27 10:36:51 +0100

    Add build script for 2024 samples

---
+++ build_scripts/test_build_2024.sh
@@ -0,0 +1,82 @@
+#!/usr/bin/env bash
+
+
+main () {
+    # get paths of the script and its directory
+    local this_file="$( echo "${BASH_SOURCE[0]:-${0}}" )"
+    local this_dir="$( cd "$( dirname "${this_file}" )" && pwd )"
+
+    # get input parameters from the command line
+    local samples="${1:-all}"
+    local channels="${2:-all}"
+    local debug="${3:-false}"
+    local steps="${4:-all}"
+
+    # get the CROWN directories
+    local crown_dir="$( cd "${this_dir}/../../.." && pwd )"
+    local crown_build_dir="${crown_dir}/build"
+    local crown_bin_dir="${crown_build_dir}/bin"
+
+    # create the build directory if it does not exist
+    if [[ ! -d "${crown_build_dir}" ]]; then
+        mkdir -p "${crown_build_dir}"
+    fi
+
+    # define fixed parameters of the compilation process
+    local analysis="xyh_bbtautau"
+    local config="nmssm_config"
+    local era="2024"
+    local shifts="none"
+    local threads="$( [[ "${debug}" == true ]] && echo "1" || echo "4" )"
+    local cores="16"
+
+    # split the samples string into an array
+    if [[ "${samples}" == "all" ]]; then
+        samples="data,nmssm_Ybb,ttbar,dyjets_amcatnlo_ll"
+    fi
+    declare -a samples_list
+    IFS="," read -ra samples_list <<< "${samples}"
+
+    # set scopes if 'all' is specified
+    if [[ "${channels}" == "all" ]]; then
+        channels="et,mt,tt"
+    fi
+
+    # build associative array of test files for different sample types
+    declare -A test_files_list
+    test_files_list[data]="root://xrootd-cms.infn.it///store/data/Run2024C/Muon0/NANOAOD/MINIv6NANOv15-v1/2530000/677e3bb0-8199-4ffb-83af-165410a7b7a6.root"
+    #test_files_list[nmssm_Ybb]=""
+    test_files_list[ttbar]="root://xrootd-cms.infn.it////store/mc/RunIII2024Summer24NanoAODv15/TTto2L2Nu_TuneCP5_13p6TeV_powheg-pythia8/NANOAODSIM/150X_mcRun3_2024_realistic_v2-v3/2810000/f60b4a6c-2801-43b0-b542-6d933a71a396.root"
+    test_files_list[dyjets_amcatnlo_ll]="root://xrootd-cms.infn.it////store/mc/RunIII2024Summer24NanoAODv15/DYto2E-2Jets_Bin-MLL-50_TuneCP5_13p6TeV_amcatnloFXFX-pythia8/NANOAODSIM/150X_mcRun3_2024_realistic_v2-v4/2540000/4dd4d960-192a-47b9-9bb4-7a6513c1a1de.root"
+
+    # configure and compile the project
+    if [[ "${steps}" == "build" || "${steps}" == "all" ]]; then
+        (
+            cd "${crown_build_dir}" \
+            && cmake .. -DANALYSIS="${analysis}" -DCONFIG="${config}" -DERAS="${era}" -DSAMPLES="${samples}" -DSCOPES="${channels}" -DSHIFTS="${shifts}" -DTHREADS="${threads}" -DDEBUG="${debug}" \
+            && make -j "${cores}" \
+            && make install
+        ) || return "${?}"
+    fi
+
+    # test the compiled binary with a test file for each declared sample
+    if [[ "${steps}" == "run_binary" || "${steps}" == "all" ]]; then
+        for sample in "${samples_list[@]}"; do
+            if [[ -n "${test_files_list[${sample}]}" ]]; then
+                # specify paths of the input and the output files
+                test_file="${test_files_list[${sample}]}"
+                output_file="output_${sample}.root"
+
+                # test the binary
+                (
+                    cd "${crown_bin_dir}" \
+                    && "./${config}_${sample}_${era}" "${output_file}" "${test_file}"
+                ) || return "${?}"
+
+            fi
+        done
+    fi
+}
+
+
+main "${@}"
