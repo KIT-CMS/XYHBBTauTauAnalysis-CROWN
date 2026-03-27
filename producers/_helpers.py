a078ef6af3da4acd5105c494f147bb73573c3f5d
moritzmolch <moritz.molch@kit.edu>
2026-03-27 11:36:16 +0100

    Add function to create producers for stepwise JEC

--- producers/_helpers.py
+++ producers/_helpers.py
@@ -3,6 +3,186 @@ from code_generation.producer import Producer, ProducerGroup
 from typing import Tuple
 
 
+
+def stepwise_jerc_producer_factory(
+    input: dict[str, Quantity],
+    output: dict[str, Quantity],
+    scopes: list[str],
+    producer_prefix: str = "Jet",
+    config_parameter_prefix: str = "jet",
+) -> Tuple[ProducerGroup, ProducerGroup, ProducerGroup]:
+    """
+    Factory function to create producers needed for jet energy corrections.
+
+    Parameters
+    ----------
+
+    input: Dict[str, Quantity]
+
+        A dictionary with the input quantities.
+
+        The `input` dictionary must have the following keys with the values being the corresponding :py:class:`~code_generation.quantity.Quantity` objects:
+
+        -  `jet_pt`: The transverse momentum of the jets with the nominal JEC applied in the `NanoAOD` production.
+        -  `jet_eta`: The pseudorapidity of the jets.
+        -  `jet_phi`: The azimuthal angle of the jets.
+        -  `jet_mass`: The mass of the jets with the nominal JEC applied in the `NanoAOD` production.
+        -  `jet_area`: The effective jet area.
+        -  `jet_raw_factor`: The factor to get the uncorrected jet energy from the jet energy with the nominal JEC applied in the `NanoAOD` production.
+        -  `jet_id`: The identification working point that the jets have passed.
+        -  `gen_jet_pt`: The transverse momentum of particle-level jets.
+        -  `gen_jet_eta`: The pseudorapidity of particle-level jets.
+        -  `gen_jet_phi`: The azimuthal angle of particle-level jet.
+        -  `rho`: The average energy density in the event.
+        -  `luminosity_block`: The luminosity block number.
+        -  `run`: The run number.
+        -  `event`: The event number.
+
+    output: Dict[str, Quantity]:
+
+        A dictionary with the output quantities that will be produced by the producers.
+
+        The `output` dictionary must have the following keys with the values being corresponding :py:class:`~code_generation.quantity.Quantity` objects:
+
+        - `jet_pt_corrected`: The corrected transverse momentum of the jets.
+        - `jet_mass_corrected`: The corrected mass of the jets.
+
+    scopes: List[str]:
+
+        List of analysis scopes, in which the JEC and JER are processed.
+
+    producer_prefix: str, default: "Jet"
+
+        Prefix used for the producer names.
+
+    config_parameter_prefix: str
+
+        Prefix used for the configuration parameter keys, to which producers in this function refer. The values of these parameters must be specified in the main configuration of the analysis.
+
+        The following configuration parameters must be defined in order to use the producers in this function:
+
+        - `{config_parameter_prefix}_jec_file`: The path to the file containing the JEC/JER corrections (in `data/` or `payloads/`).
+        - `{config_parameter_prefix}_jes_tag`: The tag in the correction file that should be used for JEC of the simulation.
+        - `{config_parameter_prefix}_jes_tag_data`: The tag in the correction file that should be used for JEC of the data.
+        - `{config_parameter_prefix}_jer_tag`: The tag in the correction file that should be used for JER of the simulation.
+        - `{config_parameter_prefix}_jec_algo`: The pileup mitigation algorithm that has been used for the jets (e.g. `AK4chs`, `AK8PFPuppi`).
+        - `{config_parameter_prefix}_reapply_jes`: Flag whether to reapply the nominal JEC. The nominal JEC has already been performed in the `NanoAOD` production.
+        - `{config_parameter_prefix}_jes_sources`: Uncertainty sources to be considered for a JEC/JER shift.
+        - `{config_parameter_prefix}_jes_shift`: Name of the systematic shift that should be applied to the JEC.
+        - `{config_parameter_prefix}_jer_shift`: Name of the systematic shift that should be applied to the JER.
+        - `{config_parameter_prefix}_jer_master_seed`: Master seed for generating the seeds for JER smearing.
+
+        LHC run number for which the JEC/JER corrections should be applied. Must be either `2` or `3`.
+
+    Returns
+    -------
+
+    Tuple[ProducerGroup, ProducerGroup, ProducerGroup]:
+
+        A tuple containing three :py:class:`~code_generation.producer.ProducerGroup` objects with the following names:
+
+        - `{producer_prefix}EnergyCorrectionData`: The first element is the producer group for the jet energy corrections on data.
+        - `{producer_prefix}EnergyCorrectionMC`: The second element is the producer group for the jet energy corrections on simulation.
+        - `{producer_prefix}EnergyCorrectionEmb`: The third element is the producer group for the jet energy corrections on embedded events.
+    """
+
+    # Get output variables from dictionary
+    jet_pt = input["jet_pt"]
+    jet_eta = input["jet_eta"]
+    jet_phi = input["jet_phi"]
+    jet_area = input["jet_area"]
+    jet_id = input["jet_id"]
+    jet_seed = input["jet_seed"]
+    genjet_pt = input["genjet_pt"]
+    genjet_eta = input["genjet_eta"]
+    genjet_phi = input["genjet_phi"]
+    rho = input["rho"]
+
+    # Get output variables from dictionary
+    jet_jec_result = output["jet_jec_result"]
+    jet_l1_pt = output["jet_l1_pt"]
+    jet_l2rel_pt = output["jet_l2rel_pt"]
+    jet_l2l3res_pt = output["jet_l2l3res_pt"]
+    jet_corrected_pt = output["jet_corrected_pt"]
+
+    # Jet pt correction for jets in data
+    jet_pt_correction_data_producer = Producer(
+        name=f"{producer_prefix}PtCorrectionData",
+        call=f"""
+        physicsobject::jet::jec::PtCorrectionData(
+            {{df}},
+            correctionManager,
+            {{output}},
+            {{input}},
+            "{{{config_parameter_prefix}_jec_file}}",
+            "{{{config_parameter_prefix}_jec_algo}}",
+            "{{{config_parameter_prefix}_jes_tag}}",
+            {{{config_parameter_prefix}_reapply_jes}},
+            "{{era}}"
+        )
+        """,
+        input=[
+            jet_pt,
+            jet_eta,
+            jet_phi,
+            jet_area,
+            rho,
+        ],
+        output=[
+            jet_jec_result,
+            jet_l1_pt,
+            jet_l2rel_pt,
+            jet_l2l3res_pt,
+            jet_corrected_pt,
+        ],
+        scopes=scopes,
+    )
+
+    # Jet pt correction for jets in MC
+    jet_pt_correction_mc_producer = Producer(
+        name=f"{producer_prefix}PtCorrectionMC",
+        call=f"""
+        physicsobject::jet::jec::PtCorrectionMC(
+            {{df}},
+            correctionManager,
+            {{output}},
+            {{input}},
+            "{{{config_parameter_prefix}_jec_file}}",
+            "{{{config_parameter_prefix}_jec_algo}}",
+            "{{{config_parameter_prefix}_jes_tag}}",
+            "{{{config_parameter_prefix}_jer_tag}}",
+            {{{config_parameter_prefix}_jes_sources}},
+            {{{config_parameter_prefix}_jes_shift_factor}},
+            "{{{config_parameter_prefix}_jer_shift}}",
+            {{{config_parameter_prefix}_reapply_jes}},
+            "{{era}}"
+        )
+        """,
+        input=[
+            jet_pt,
+            jet_eta,
+            jet_phi,
+            jet_area,
+            jet_id,
+            genjet_pt,
+            genjet_eta,
+            genjet_phi,
+            rho,
+            jet_seed,
+        ],
+        output=[
+            jet_jec_result,
+            jet_l1_pt,
+            jet_l2rel_pt,
+            jet_l2l3res_pt,
+            jet_corrected_pt,
+        ],
+        scopes=scopes,
+    )
+
+    return jet_pt_correction_data_producer, jet_pt_correction_mc_producer
+
+
 def jerc_producer_factory(
     input: dict[str, Quantity],
     output: dict[str, Quantity],
