e441faae2e7433d33035d1ffd6d04300a768852f
moritzmolch <moritz.molch@kit.edu>
2026-05-06 13:42:35 +0200

    Transfer changes from https://github.com/KIT-CMS/CROWN/pull/368 to cpp_addons

---
+++ cpp_addons/include/met.hxx
@@ -0,0 +1,25 @@
+#ifndef GUARDMETEXT_H
+#define GUARDMETEXT_H
+
+#include "ROOT/RDataFrame.hxx"
+
+
+namespace met {
+
+ROOT::RDF::RNode Type1Correction(
+    ROOT::RDF::RNode df,
+    const std::string &outputname,
+    const std::string raw_met,
+    const std::string &t1jet_pt_l1corrected,
+    const std::string &t1jet_pt_corrected,
+    const std::string &t1jet_eta,
+    const std::string &t1jet_phi,
+    const std::string &t1jet_em_ef,
+    const float &t1jet_min_pt,
+    const float &t1jet_max_abs_eta,
+    const float &t1jet_max_em_ef
+);
+
+}
+
+#endif
